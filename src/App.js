c487135c56127b0dd01654d0a58ad31d8803c5d3
Satyam Tiwari <satyamg.dtg@gmail.com>
2021-03-27 22:38:33 +0530

    Added New Movie Form

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Customers from "./components/customers";
 import NavBar from "./components/navBar";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 import "./App.css";
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Switch>
           <Route path="/movies/:id" component={MovieForm} />
           <Route path="/login" component={LoginForm} />
+          <Route path="/register" component={RegisterForm} />
           <Route path="/movies" component={Movies}></Route>
           <Route path="/customers" component={Customers}></Route>
           <Route path="/rentals" component={Rentals}></Route>
